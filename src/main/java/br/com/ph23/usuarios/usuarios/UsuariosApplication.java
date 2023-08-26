be46499c8abfae5fcd6b1c2e3baee97328a28c45
Paulo Henrique Andrade Alves <80326186+PhAlves23@users.noreply.github.com>
2023-08-26 19:35:03 -0300

    feat: start project

---
+++ src/main/java/br/com/ph23/usuarios/usuarios/UsuariosApplication.java
@@ -0,0 +1,13 @@
+package br.com.ph23.usuarios.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ph23/usuarios/usuarios/UsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ph23.usuarios.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
